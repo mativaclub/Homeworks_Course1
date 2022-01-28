631d3bad5e48e87a434f9e89d382f231dcdd5155
mativaclub <96348077+mativaclub@users.noreply.github.com>
2022-01-28 20:11:42 +0200

    all homeworks

--- src/homework_9_Objects_Methods/Objects/Methods/main.java
+++ src/homework_9_Objects_Methods/Objects/Methods/main.java
@@ -1,4 +1,29 @@
 package homework_9_Objects_Methods.Objects.Methods;
 
 public class main {
+
+    public static void main(String[] args) {
+
+        Book book1 = new Book("Иди туда где страшно",
+                new Author("Джим", "Лоулесс"), 2017);
+        book1.setYearOfPublication(2020);
+        System.out.println(book1);
+
+        Book book2 = new Book("Исповедь экономического убийцы",
+                new Author("Джон", "Перкинс"), 2018);
+        System.out.println(book2);
+
+
+        Book book3 = new Book("Исповедь экономического убийцы",
+                new Author("Джон", "Перкинс"), 2018);
+        System.out.println(book3);
+
+        System.out.println(book1.equals(book3));
+        System.out.println(book2.equals(book3));
+
+        System.out.println(new Author("John", "Perkins").hashCode());
+        System.out.println(new Book("Исповедь экономического убийцы",
+                new Author("John", "Perkins"),
+                2018).hashCode());
+    }
 }
